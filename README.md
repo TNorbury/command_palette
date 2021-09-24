a12c67d7b4d88256655685121b2e10e88b689285
Tyler Norbury <tylernorbury96@gmail.com>
2021-09-23 19:14:21 -0700

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# command_bar
+
+A Flutter widget that allows you to bring up a command bar (or command pallette), seen in programs like Visual Studio Code and Slack.
+Allows you to provide users with a convenient way to perform all sorts of actions related to your app.
+
+
+## Features
+
+* Access the command-bar via a keyboard shortcut, or programmatically.
+* Define a custom list of options for the user and define the callbacks for each.
+* Use the default styling or build your own custom list items. 
+
+
+## Getting started
+
+TODO: List prerequisites and provide or point to information on how to
+start using the package.
+
+## Usage
+
+TODO: Include short and useful examples for package users. Add longer examples
+to `/example` folder. 
+
+```dart
+const like = 'sample';
+```
+
+## Additional information
+
+TODO: Tell users more about the package: where to find more information, how to 
+contribute to the package, how to file issues, what response they can expect 
+from the package authors, and more.
---
+++ example/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Command Bar Example',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text("Welcome to the Command Bar example"),
+          ],
+        ),
+      ),
+    );
+  }
+}
